22782c484b2dcc9980f32181777028a0ad165a5a
uchedotphp <uchedotphp@gmail.com>
2020-08-18 14:45:58 +0100

    added google fonts of Poppins to App.vue

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,7 @@ export default {
 </script>
 
 <style>
+@import url("https://fonts.google.com/specimen/Poppins");
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
